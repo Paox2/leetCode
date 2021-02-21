d4ccf8b687b08ec09975c92cac71c120dbf52e2e
Wendi HAN <scywh1@exmail.nottingham.ac.uk>
2021-02-21 15:31:19 +0000

    991 java

---
+++ 991. Broken Calculator/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        if (X >= Y) {
+            return X - Y;
+        }
+        
+        int times = 0;
+        while (X < Y) {
+            times += 1;
+            
+            if (Y % 2 == 0)
+                Y /= 2;
+            else
+                Y++;
+        }
+        
+        return times + X - Y;
+    }
+};
