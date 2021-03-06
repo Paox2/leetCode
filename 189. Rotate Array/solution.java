caf4eca72179b27d29ccefd9c28407f1b51a0caf
Wendi HAN <wendihan493@gmail.com>
2021-03-06 17:28:43 +0000

    189

---
+++ 189. Rotate Array/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int i = 0;
+        int length = nums.length;
+        if ((k = k % length) == 0) {
+            return;
+        }
+        
+        reverse(nums, 0, length - 1);
+        reverse(nums, 0, (k - 1));
+        reverse(nums, k, length - 1);       
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        while(start < end){
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+}
